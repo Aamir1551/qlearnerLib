3ae825cc95cf7cacb41b0de5ceb72668c38d3432
aamir soni <ll17a26s@leeds.ac.uk>
2019-06-20 22:12:27 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function qtable(numStates, numAction) {
-  this.table = Array(numStates).fill(Array(numAction).fill(0));
+    this.table = Array(numStates).fill(Array(numAction).fill(0));
 }
 
 qtable.updateCell = function(l_r, gamma, reward, state, action, newState) {
-  this.table[state][action] += l_r * (reward + gamma * Math.max(...this.table[newState] - this.table[state][action]));
+    this.table[state][action] += l_r * (reward + gamma * Math.max(...this.table[newState] - this.table[state][action]));
 }
 
 //actionMap is an array of the actions that can be done
-function agent(_actionMap,  _getNewState, _isActionValid) {
-  this.actionMap = _actionMap;
-  this.getNewState = _getNewState;
-  this.isActionValid = _isActionValid;
+function agent(initialState, _actionMap,  _getNewState, _isActionValid, _actionSet) {
+    this.state = initialState;
+    this.actionMap = _actionMap;
+    this.getNewState = _getNewState;
+    this.isActionValid = _isActionValid;
+    this.actionSet = _actionSet;
 }
 
 
 //actionProbs is an array contianing probability of actions
-agent.prototype.performAction(actionsProbs) = function() {
+agent.prototype.chooseAction(actionsProbs) = function() {
   let sumed = actionsProbs.reduce((a, b)=>a+b);
   let c = sumed * Math.random();
   let actionChosen = -1;
@@ -27,7 +29,7 @@ agent.prototype.performAction(actionsProbs) = function() {
 }
 
 function checkIsValidMovement(state, action) {
-  return this.actionMap[state[0]][state[1]][1].indexOf(action) != -1;
+    return this.actionMap[state.toString()].indexOf(action) != -1;
 }
 
 map = [
@@ -42,22 +44,27 @@ map = [
   ]
 ]
 
+//for more complicated environments, the possible actions can be created via a function (same with rewards)
 actionMap = { 
-  1: [ 'd', 'r' ], 
-  2: [ 'l', 'r', 'd' ], 
-  3:[ 'l', 'd' ],
-  4: [ 'u', 'r', 'd' ],
-  5: [ 'u', 'r', 'l', 'd' ],
-  6: [ 'u', 'l', 'd' ],
-  7: [ 'u', 'r' ],
-  8: [ 'u', 'r', 'l' ],
-  9: [] }
+    '0,0': [ 'd', 'r' ], 
+    '1,0': [ 'l', 'r', 'd' ], 
+    '2,0': [ 'l', 'd' ],
+    '0,1': [ 'u', 'r', 'd' ],
+    '1,1': [ 'u', 'r', 'l', 'd' ],
+    '2,1': [ 'u', 'l', 'd' ],
+    '0,2': [ 'u', 'r' ],
+    '1,2': [ 'u', 'r', 'l' ],
+    '2,2': []
+}
 
 rewards = {
-  9: 0
+    '2,2': 0
 }
 
 function getNextState(action, state) { //this function is allowed to interact with the environment
+  if(this.actionMap[state.toString].indexOf(action) == -1) {
+      return "invalid state";
+  }
   if(action == 'd') {
     state[1]+=3;
   } else if(action=='u') {
@@ -67,31 +74,31 @@ function getNextState(action, state) { //this function is allowed to interact wi
   } else if(action=='l') {
     state[0]-=1;
   }
-  if(state[0] < 0 || state[1] < 0) { //this is bad (in future we should check if the action is ALLOWED within the state (and not see if the state we ended up in is valid))
-    return "invalid state";
-  }
   return state;
 }
 
 
 brain = new qtable(3, 4); // [r, l, u, d]
-player = new agent(map, getNextState, checkIsValidMovement);
+player = new agent([0,0], actionMap, getNextState, checkIsValidMovement, ['r', 'l', 'u', 'd']);
+
 function playgame(player, gameSteps, exp_rate) {
-  x = 0;
-  y = 0;
+    
+    for(let i=0; i<gameSteps; i++) {
+        
+        actions = brain.table[y*3+x-1];
+        current_state = player.actionMap
+        
+        if(Math.random() > exp_rate) {
+            //pick best action
+            do {
+                actionChosen = actions.indexOf(Math.max(...actions));
+            } while(player.checkIsValidMovement(player.state, actionChosen));
+        
+        } else {
 
-  for(let i=0; i<gameSteps; i++) {
-    actions = brain.table[y*3+x-1];
-    current_state = player.actionMap[;
-
-    if(Math.random() > exp_rate) {
-      do {
-        actionChosen = actions.indexOf(Math.max(...actions));
-      } while(player.checkIsValidMovement([x, y], actionChosen));
-    } else {
-      //perform random action
-      player.performAction()
-      actionChosen = player.actionMap[y*3+x-1][1]
+            //perform random action
+            player.performAction()
+            actionChosen = player.actionMap[y*3+x-1][1]
 
 
     }
