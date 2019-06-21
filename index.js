fd10da7c6c0c326da191e64ed020f7d7ffff8e8b
aamir soni <ll17a26s@leeds.ac.uk>
2019-06-21 17:46:50 +0100

    project completed

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ qtable.prototype.getBestActionForState = function(state, legalActions) {
 
 //actionMap is an array of the actions that can be done
 function agent(initialState, _actionMap,  _getNewState) {
-    this.state = initialState;
-    this.actionMap = _actionMap;
-    this.getNewState = _getNewState;
+  this.state = initialState;
+  this.actionMap = _actionMap;
+  this.getNewState = _getNewState;
 }
 
 agent.prototype.getStateID = function(state) {
@@ -43,26 +43,25 @@ agent.prototype.getLegalActions = function() {
 //for more complicated environments, the possible actions can be created via a function (same with rewards)
 // [0=d, 1=u, 2=r, 3=l]
 actionMap = { 
-    '0,0': [ '0', '2' ], 
-    '1,0': [ '3', '2', '0' ], 
-    '2,0': [ '3', '0' ],
-    '0,1': [ '1', '2', '0' ],
-    '1,1': [ '1', '2', '3', '0' ],
-    '2,1': [ '1', '3', '0' ],
-    '0,2': [ '1', '2' ],
-    '1,2': [ '1', '2', '3' ],
-    '2,2': []
+  '0,0': [ '0', '2' ], 
+  '1,0': [ '3', '2', '0' ], 
+  '2,0': [ '3', '0' ],
+  '0,1': [ '1', '2', '0' ],
+  '1,1': [ '1', '2', '3', '0' ],
+  '2,1': [ '1', '3', '0' ],
+  '0,2': [ '1', '2' ],
+  '1,2': [ '1', '2', '3' ],
+  '2,2': []
 }
 
 rewards = {
-    '2,2': 100
+  '2,2': 100
 }
 
 function getNextState(action, state) { //this function is allowed to interact with the environment
   if(this.actionMap[state.toString()].indexOf(action) == -1) {
-      return "invalid state";
+    return "invalid state";
   }
-
   if(action == '0') {
     state[1]+=1;
   } else if(action=='1') {
@@ -80,31 +79,27 @@ brain = new qtable(9, 4); // [r, l, u, d]
 player = new agent([0,0], actionMap, getNextState);
 
 function playgame(player, gameSteps, exp_rate) {
-    player.state = [0,0]; 
-    for(let i=0; i<gameSteps; i++) {
-        if(player.state.toString() === [2, 2].toString()) {
-          break;
-        }
-        let currentState = player.state.toString();
-        legalActions = player.getLegalActions();
-        
-        if(Math.random() > exp_rate) {
-            actionChosen = brain.getBestActionForState(player.getStateID(player.state), legalActions);
-            console.log(player.state, actionChosen);
-        } else {
-            //perform random action
-            actionChosen = player.chooseRandomAction();
-        }
-        player.state = player.getNewState(actionChosen, player.state);
-        rewardRecieved = rewards[player.state] ? rewards[player.state] : 0;
-        brain.updateCell(0.98, 0.1, rewardRecieved, player.getStateID(currentState.toString()), actionChosen, player.getStateID(player.state.toString()));
-        
+  player.state = [0,0]; 
+  for(let i=0; i<gameSteps; i++) {
+    if(player.state.toString() === [2, 2].toString()) {
+      break;
+    }
+    let currentState = player.state.toString();
+    legalActions = player.getLegalActions();
+    if(Math.random() > exp_rate) {
+      actionChosen = brain.getBestActionForState(player.getStateID(player.state), legalActions);
+      console.log(player.state, actionChosen);
+    } else {
+      actionChosen = player.chooseRandomAction();
+    }
+    player.state = player.getNewState(actionChosen, player.state);
+    rewardRecieved = rewards[player.state] ? rewards[player.state] : 0;
+    brain.updateCell(0.98, 0.1, rewardRecieved, player.getStateID(currentState.toString()), actionChosen, player.getStateID(player.state.toString()));
   }
-
 }
 
 for(let run=0; run<100; run++) {
-    playgame(player, 6, 1);
+  playgame(player, 6, 1);
 }
 
 cheatSheet = [];
